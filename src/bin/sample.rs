07495876636e81a0a4d076c6749dff0bff1143ed
sheroz <sheroz@dreamlogic.io>
2023-07-04 17:59:54 +0300

    removed openssl sample

--- src/bin/sample.rs
+++
@@ -1,9 +0,0 @@
-use openssl::sha::sha256;
-
-fn main() {
-    let txt = "Hello world!";
-    let hash = sha256(txt.as_bytes());
-    let digest = hex::encode(hash);    
-    assert_eq!(digest, "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
-    println!("\nOpenSSL: SHA256 hash for {}\n{}\n", txt, digest);
-}
