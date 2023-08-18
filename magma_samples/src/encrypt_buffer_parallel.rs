ed05eef480364776fe7d4ea9b5510a65ffc646f8
Sheroz <sheroz.khaydarov@gmail.com>
2023-08-18 23:57:40 +0300

    added parallel processing sample

--- magma_samples/src/encrypt_buffer_parallel.rs
+++ magma_samples/src/encrypt_buffer_parallel.rs
@@ -1,14 +1,25 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
-/// Sample of buffer encryption by parallel processing
+/// Sample of buffer encryption by parallel processing of chunks
+/// Results (MacBook Pro M1 2021):
+/// 
+/// ```text
+/// Source len: 16850000
+/// Encrypting by parallel processing...
+/// Encryption, elapsed ticks: 19012211
+/// Encrypted len: 16850000
+/// Decrypting in single thread...
+/// Decryption, elapsed ticks: 156965972
+/// Parallel processing speedup: 8.256060907382103
+/// Decrypted len: 16850000
+/// Decrypted final len: 16850000
+/// Completed.
+/// ```
 pub fn encrypt_buffer_parallel() {
     use cipher_magma::{CipherMode, MagmaStream};
     use rayon::prelude::*;
+    use std::collections::HashMap;
+    use std::sync::{Arc, Mutex};
 
-    const CHUNK_SIZE: usize = 128;
+    const CHUNK_SIZE: usize = 4096;
 
     let key = [0xab; 32];
     let mut magma = MagmaStream::new(key, CipherMode::CTR);
@@ -20,29 +31,22 @@ pub fn encrypt_buffer_parallel() {
         Quisque iaculis est et est volutpat posuere.\n";
 
     // build the source buffer containing 50000x of txt ~ 16 MB
-    let repeat_count = 1;
+    let repeat_count = 50000;
     let mut source = Vec::<u8>::with_capacity(txt.len() * repeat_count);
     (0..repeat_count).for_each(|_| source.extend_from_slice(txt));
-    println!("Source len:{}", source.len());
+    println!("Source len: {}", source.len());
 
-    let chunks = source.chunks(CHUNK_SIZE);
-    println!("Chunk size:{}", CHUNK_SIZE);
-    println!("Chunks count:{}", chunks.clone().count());
-
-    println!("Encrypting...");
+    println!("Encrypting by parallel processing...");
     let mut encrypted = Vec::<u8>::with_capacity(source.len());
-
+    let counter_alignment = if CHUNK_SIZE % 8 == 0 { 0 } else { 1 };
     let mutex = Arc::new(Mutex::new(HashMap::<usize, Vec<u8>>::new()));
+    let encrypt_start = tick_counter::start();
     source
         .par_chunks(CHUNK_SIZE)
         .enumerate()
         .for_each(|(index, chunk)| {
-            let mut counter = (index * CHUNK_SIZE) as u64;
-            if counter > 0 {
-                counter -= 1;
-            }
-
-            let (ciphertext, _) = cipher_magma::ctr::cipher_ctr_core(&magma, chunk, counter);
+            let counter = counter_alignment + (index * CHUNK_SIZE / 8) as u64;
+            let (ciphertext, _counter) = cipher_magma::ctr::cipher_ctr_core(&magma, chunk, counter);
             mutex.lock().unwrap().insert(index, ciphertext);
         });
 
@@ -54,33 +58,32 @@ pub fn encrypt_buffer_parallel() {
         .iter()
         .for_each(|index| encrypted.append(map.get_mut(index).unwrap()));
 
-    println!("Encrypted len:{}", encrypted.len());
+    let encrypt_ticks = tick_counter::stop() - encrypt_start;
+    println!("Encryption, elapsed ticks: {}", encrypt_ticks);
+    println!("Encrypted len: {}", encrypted.len());
 
-    println!("Decrypting...");
+    println!("Decrypting in single thread...");
     let mut decrypted = Vec::<u8>::with_capacity(encrypted.len());
     let encrypted_chunks = encrypted.chunks(CHUNK_SIZE);
+    let decrypt_start = tick_counter::start();
     for chunk in encrypted_chunks {
         let mut plaintext = magma.decrypt(&chunk);
         decrypted.append(&mut plaintext);
     }
-    println!("Decrypted len:{}", encrypted.len());
+    let decrypt_ticks = tick_counter::stop() - decrypt_start;
+    let speedup = (decrypt_ticks as f64) / (encrypt_ticks as f64);
+    println!("Decryption, elapsed ticks: {}", decrypt_ticks);
+    println!("Parallel processing speedup: {}", speedup);
+
+    println!("Decrypted len: {}", encrypted.len());
 
     // remove padding bytes
     if magma.get_mode().has_padding() {
         decrypted.truncate(source.len());
     }
-    println!("Decrypted final len:{}", encrypted.len());
+    println!("Decrypted final len: {}", encrypted.len());
 
     assert_eq!(decrypted, source);
 
     println!("Completed.");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn encrypt_buffer_parallel_test() {
-        encrypt_buffer_parallel();
-    }
 }
--- magma_samples/src/main.rs
+++ magma_samples/src/main.rs
@@ -2,8 +2,8 @@ mod calculate_mac;
 mod encrypt_block;
 mod encrypt_bmp;
 mod encrypt_buffer;
+mod encrypt_buffer_parallel;
 mod encrypt_file;
-mod encrypt_parallel;
 mod encrypt_text;
 
 fn main() {
@@ -13,13 +13,16 @@ fn main() {
     println!("\n***\n\nText encryption\n");
     encrypt_text::encrypt_text();
 
-    println!("\n***\n\nEncrypting of large buffer in chunks\n");
+    println!("\n***\n\nEncrypting buffer in chunks\n");
     encrypt_buffer::encrypt_buffer();
+
+    println!("\n***\n\nEncrypting buffer by parallel processing of chunks\n");
+    encrypt_buffer_parallel::encrypt_buffer_parallel();
 
     println!("\n***\n\nMessage Authentication Code (MAC) calculation\n");
     calculate_mac::calculate_mac();
 
-    println!("\n***\n\nMessage Authentication Code (MAC) calculation in data chunks\n");
+    println!("\n***\n\nMessage Authentication Code (MAC) calculation in chunks\n");
     calculate_mac::calculate_mac_stream();
 
     println!("\n***\n\nFile encryption\n");
